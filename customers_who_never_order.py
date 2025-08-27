2770d72c4b19511e697e21c4a07f2f05fcfa0935
jguevaraangel <meur40@gmail.com>
2025-08-27 13:10:46 +0200

    Customers who never order problem

---
+++ customers_who_never_order.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+"""
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| name        | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table indicates the ID and name of a customer.
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| customerId  | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+customerId is a foreign key (reference columns) of the ID from the Customers table.
+Each row of this table indicates the ID of an order and the ID of the customer who ordered it.
+"""
+
+def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
+    ordered_customers = orders['customerId'].unique()
+    return customers[~customers['id'].isin(ordered_customers)][['name']].rename(columns={'name': 'Customers'})
