12c118e3a42a916aae7b99eb99af6cfc7e77c584
jguevaraangel <meur40@gmail.com>
2025-09-02 17:26:37 +0200

    Replace employee ID with the unique identifier problem

---
+++ replace_employee_id_with_unique_identifier.py
@@ -0,0 +1,32 @@
+import pandas as pd
+
+"""
+Table: Employees
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| name          | varchar |
++---------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains the id and the name of an employee in a company.
+
+ 
+
+Table: EmployeeUNI
+
++---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| id            | int     |
+| unique_id     | int     |
++---------------+---------+
+(id, unique_id) is the primary key (combination of columns with unique values) for this table.
+Each row of this table contains the id and the corresponding unique id of an employee in the company.
+
+"""
+
+def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
+    merged_df = employees.merge(employee_uni, on='id', how='left')
+    return merged_df[['unique_id', 'name']]    
